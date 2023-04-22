d38c303b690220defc98d9b232c3ba9aa6a5f448
TetrisUnblocked <104165736+Chipotleguest@users.noreply.github.com>
2023-04-21 20:32:22 -0500

    Update fullscreen.js

--- choppy-orc/fullscreen.js
+++ choppy-orc/fullscreen.js
@@ -1,27 +1,12 @@
-/**
- * This function takes an iframe element and makes it go fullscreen when the link is opened.
- * 
- * @param {string} iframeId - The id of the iframe element
- */
-function openFullscreen(iframeId) {
-  try {
-    // Get the iframe element
-    const iframe = document.getElementById(iframeId);
-    
-    // Check if the iframe exists
-    if (!iframe) {
-      throw new Error("Iframe element not found");
+(function() {
+  var iframe = document.getElementsByTagName('iframe')[0];
+  if (iframe) {
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    } else if (iframe.webkitRequestFullscreen) {
+      iframe.webkitRequestFullscreen();
+    } else if (iframe.mozRequestFullScreen) {
+      iframe.mozRequestFullScreen();
     }
-    
-    // Check if the iframe is already in fullscreen mode
-    if (document.fullscreenElement === iframe) {
-      throw new Error("Iframe is already in fullscreen mode");
-    }
-    
-    // Request fullscreen mode for the iframe
-    iframe.requestFullscreen();
-  } catch (error) {
-    // Log the error
-    console.error(error);
   }
-}
+})();
