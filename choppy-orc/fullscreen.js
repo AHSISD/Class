930e08be881e5b013f4c44a1a3db8021bc51fb9d
TetrisUnblocked <104165736+Chipotleguest@users.noreply.github.com>
2023-04-21 19:36:46 -0500

    szfd

--- choppy-orc/fullscreen.js
+++ choppy-orc/fullscreen.js
@@ -1,27 +1,39 @@
 /**
- * This function creates a fullscreen button on an iframe.
- * When the button is clicked, the iframe will be displayed in fullscreen mode.
- *
- * @param {string} ChoppyOrc  - The ID of the iframe element.
+ * This function creates a fullscreen button with an image on an iframe.
+ * 
+ * @param {string} ChoppyOrc - The ID of the iframe element.
+ * @param {string} https://ibb.co/7SH2bbH - The URL of the image to be displayed on the button.
  */
-function createFullscreenButton(ChoppyOrc) {
-  const iframe = document.getElementById(ChoppyOrc);
-
-  // Create the fullscreen button
-  const fullscreenButton = document.createElement('button');
-  fullscreenButton.textContent = 'Fullscreen';
-
-  // Add a click event listener to the button
-  fullscreenButton.addEventListener('click', () => {
-    if (iframe.requestFullscreen) {
-      iframe.requestFullscreen();
-    } else if (iframe.webkitRequestFullscreen) {
-      iframe.webkitRequestFullscreen();
-    } else if (iframe.msRequestFullscreen) {
-      iframe.msRequestFullscreen();
-    }
-  });
-
-  // Add the button to the iframe
-  iframe.parentNode.insertBefore(fullscreenButton, iframe);
+function createFullscreenButton(iframeId, imageUrl) {
+  try {
+    // Get the iframe element
+    const iframe = document.getElementById(iframeId);
+    
+    // Create the fullscreen button element
+    const fullscreenButton = document.createElement("button");
+    fullscreenButton.style.position = "absolute";
+    fullscreenButton.style.top = "0";
+    fullscreenButton.style.right = "0";
+    fullscreenButton.style.zIndex = "9999";
+    fullscreenButton.style.backgroundImage = `url(${imageUrl})`;
+    fullscreenButton.style.backgroundSize = "contain";
+    fullscreenButton.style.backgroundRepeat = "no-repeat";
+    fullscreenButton.style.width = "50px";
+    fullscreenButton.style.height = "50px";
+    fullscreenButton.addEventListener("click", () => {
+      // Request fullscreen for the iframe
+      if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+      } else if (iframe.webkitRequestFullscreen) {
+        iframe.webkitRequestFullscreen();
+      } else if (iframe.msRequestFullscreen) {
+        iframe.msRequestFullscreen();
+      }
+    });
+    
+    // Add the fullscreen button to the iframe
+    iframe.parentNode.insertBefore(fullscreenButton, iframe);
+  } catch (error) {
+    console.error(error);
+  }
 }
